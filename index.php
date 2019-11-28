230e50f19895f9eee6a8bfb8188d4abae777b7fe
Hans Schouten <hans@falcosolutions.nl>
2019-11-28 07:21:37 +0100

    Reverse proxy script added

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+$targetUrl = "http://localhost:5601";
+
+// init curl to $targetUrl with the requested URI
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $targetUrl . $_SERVER['REQUEST_URI']);
+
+// get post data
+$postvars = '';
+if (isset($_POST) && empty($_POST)) {
+    $rest_json = file_get_contents("php://input");
+    if ($rest_json) {
+        $postvars = $rest_json;
+        //set the content type to application/json
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type:application/json']);
+    }
+} else {
+    if (isset($_POST)) {
+        foreach ($_POST as $key => $value) {
+            $postvars .= $key . '=' . $value . '&';
+        }
+    }
+}
+
+// return the transfer as a string
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+// enable headers in response
+curl_setopt($ch, CURLOPT_HEADER, 1);
+// forward post data
+if ($postvars) {
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $postvars);
+}
+
+// execute curl request
+$response = curl_exec($ch);
+curl_close($ch);
+
+// set headers from response
+list($header, $body) = explode("\r\n\r\n", $response, 2);
+$headerLines = explode("\n",$header);
+foreach ($headerLines as $headerLine) {
+    header($headerLine);
+}
+
+echo $body;
